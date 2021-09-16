bfacf6d3d5dc3f0081ef2d68576b043e1183bdfb
Eivind Fonn <evfonn@gmail.com>
2021-09-16 10:42:29 +0200

    Types in api

--- Costa/api.py
+++ Costa/api.py
@@ -1,45 +1,52 @@
 from abc import abstractmethod, abstractproperty, ABC
 
+from typing import Dict, Union, List
+
+import numpy as np
+
+
+Parameters = Dict[str, Union[float, List[float]]]
+
 
 class PhysicsModel(ABC):
 
     @abstractproperty
-    def ndof(self):
+    def ndof(self) -> int:
         """Return the number of degrees of freedom."""
 
     @abstractmethod
-    def dirichlet_dofs(self):
+    def dirichlet_dofs(self) -> np.ndarray:
         """Return a list of Dirichlet DoF IDs (1-indexed)."""
 
     @abstractmethod
-    def predict(self, params, uprev):
+    def predict(self, params: Parameters, uprev: np.ndarray) -> np.ndarray:
         """Make an uncorrected prediction of the next timestep given the
         previous timestep.  This is nothing more than a standard discrete
         timestep method.
 
-        :param params: List of parameters.  By convention the first parameter is
-            the timestep.
+        :param params: Dictionary of parameters.
+            By convention the timestep is named 'dt'.
         :param uprev: Previous timestep.  May be ignored by a stationary solver.
         :return: Prediction of next timestep.
         """
 
     @abstractmethod
-    def residual(self, params, uprev, unext):
+    def residual(self, params: Parameters, uprev: np.ndarray, unext: np.ndarray) -> np.ndarray:
         """Calculate the residual b - Au given the assumed solution unext.
 
-        :param params: List of parameters.  By convention the first parameter is
-            the timestep.
+        :param params: Dictionary of parameters.
+            By convention the timestep is named 'dt'.
         :param uprev: Previous timestep.  May be ignored by a stationary solver.
         :param unext: The purported exact or experimental solution.
         :return: The residual b - Au."""
 
     @abstractmethod
-    def correct(self, params, uprev, sigma):
+    def correct(self, params: Parameters, uprev: np.ndarray, sigma: np.ndarray) -> np.ndarray:
         """Calculate a corrected prediction of the next timestep given
         the previous timestep and a right-hand side perturbation.
 
-        :param params: List of parameters.  By convention the first parameter is
-            the timestep.
+        :param params: Dictionary of parameters.
+            By convention the timestep is named 'dt'.
         :param uprev: Previous timestep.  May be ignored by a stationary solver.
         :param sigma: Right-hand-side perturbation. If equal to zero, this method
             should be equivalent to predict(params, uprev).
@@ -51,12 +58,12 @@ class PhysicsModel(ABC):
 class DataModel(ABC):
 
     @abstractmethod
-    def __call__(self, params, upred):
+    def __call__(self, params: Parameters, upred: np.ndarray) -> np.ndarray:
         """Calculate a right-hand side perturbation to use for a corrected
         prediction of the next timestep given the uncorrected prediction.
 
-        :param params: List of parameters.  By convention the first parameter is
-            the timestep.
+        :param params: Dictionary of parameters.
+            By convention the timestep is named 'dt'.
         :param upred: Uncorrected prediction of next timestep (return value of
             PhysicsModel.predict) - external vector.
         :return: Right-hand-side perturbation for use in PhysicModel.correct
