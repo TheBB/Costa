9ef00011e2934a1f213739973e238bf1125566c9
Eivind Fonn <evfonn@gmail.com>
2021-11-08 14:28:34 +0100

    Use a PBM in DataTrainer

--- Costa/ddm.py
+++ Costa/ddm.py
@@ -15,32 +15,26 @@ from .api import PhysicsModel, DataModel, DataTrainer
 
 class KerasTrainer(DataTrainer):
 
+    pbm: PhysicsModel
+    x: List[np.ndarray]
+    y: List[np.ndarray]
     model: Optional[Model] = None
-    x: Optional[np.ndarray] = None
-    y: Optional[np.ndarray] = None
 
-    def __init__(self, model: Optional[Model] = None):
+    def __init__(self, pbm: PhysicsModel, model: Optional[Model] = None):
+        self.pbm = pbm
         self.model = model
+        self.x, self.y = [], []
 
-    def append(self, x: np.ndarray, y: np.ndarray):
-        if x.ndim == 1:
-            x = x.reshape(1, -1)
-        if y.ndim == 1:
-            y = y.reshape(1, -1)
-        assert x.shape[0] == y.shape[0]
-        if self.x is None:
-            self.x = x
-            self.y = y
-            return
-        self.x = np.append(self.x, x, 0)
-        self.y = np.append(self.y, y, 0)
+    def append(self, params, uprev: np.ndarray, unext: np.ndarray):
+        x = np.array(self.pbm.predict(params, uprev)).reshape(1, -1)
+        y = np.array(self.pbm.residual(params, uprev, unext)).reshape(1, -1)
+        self.x.append(x)
+        self.y.append(y)
 
     def ensure_model(self, nlayers: int = 4, layer_size_factor: float = 4):
         if self.model is not None:
             return
-        assert self.x is not None
-        assert self.y is not None
-        inshape, outshape = self.x.shape[1], self.y.shape[1]
+        inshape, outshape = self.x[0].shape[0], self.y[0].shape[0]
         midshape = int(layer_size_factor * inshape)
 
         model = Sequential()
@@ -54,15 +48,20 @@ class KerasTrainer(DataTrainer):
 
     def train(self, learing_rate: float = 1e-5, **kwargs) -> Keras:
         self.ensure_model()
-        assert self.x is not None
-        assert self.y is not None
         assert self.model is not None
 
         optimizer = Adam(learning_rate=learing_rate)
         self.model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mae', 'mse'])
 
         monitor = EarlyStopping(patience=20)
-        self.model.fit(self.x, self.y, batch_size=32, epochs=100, validation_split=0.1, callbacks=[monitor])
+        self.model.fit(
+            np.array(self.x),
+            np.array(self.y),
+            batch_size=32,
+            epochs=100,
+            validation_split=0.1,
+            callbacks=[monitor],
+        )
         return Keras(self.model)
 
 
@@ -85,11 +84,6 @@ class Keras(DataModel):
 
     def save(self, filename: Union[str, Path]):
         self.model.save(filename)
-
-    def retrain(self, x: np.ndarray, y: np.ndarray, learning_rate: float = 1e-8):
-        trainer = KerasTrainer(self.model)
-        trainer.append(x, y)
-        self.model = trainer.train(learing_rate=learning_rate)
 
 
 class Omniscient(DataModel):
